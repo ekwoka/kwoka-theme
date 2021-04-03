d1c6ba2583b107fdc202c683317d6f9bf4d4d0e1
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-04-03 21:41:47 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,32 +19,15 @@ module.exports = {
         truegray: colors.trueGray,
       },
       margin: {
-        '-16': '-2rem',
-        '-32': '-4rem',
-        '-48': '-6rem',
-        '-64': '-8rem',
-        '-80': '-10rem',
-        '-96': '-12rem',
-        '-20h': '-20vh',
-        '-24h': '-24vh',
       },
       padding: {
-        '20h': '20vh',
-        '24h': '24vh',
       },
       inset: {
-        '1/10': '10%',
-        '1/20': '5%',
       },
       height: {
-        'screen-1/2': '50vh',
-        'screen-1/4': '25vh',
-        'screen-3/4': '75vh',
       },
       width: {},
       lineHeight: {
-        '12': '3rem',
-        '16': '4rem',
       },
       letterSpacing: ['dark']
     },
