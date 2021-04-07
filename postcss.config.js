ec69c7b41d87b357871b8dd2d692b774c1207ed3
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-04-07 16:12:50 +0800

    Installing Tailwind JIT

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require(`tailwindcss`)(`./tailwind.config.js`),
+    require(`@tailwindcss/jit`)(`./tailwind.config.js`),
     require(`autoprefixer`),
     ...(process.env.NODE_ENV === "production"
       ? [
