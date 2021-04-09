da4bbe41fbc87be4f6259bd9b1406f64d4a0cf0c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-04-09 18:12:56 +0800

    Inline Critical Header CSS

---
+++ tailwind.header.config.js
@@ -0,0 +1,41 @@
+const height = require("tailwindcss/defaultTheme")
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  purge: {
+    mode: "all",
+    content: ["./**/header.liquid"],
+    options: {
+      whitelist: [],
+    },
+  },
+  darkMode: 'media',
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        truegray: colors.trueGray,
+      },
+      margin: {
+      },
+      padding: {
+      },
+      inset: {
+      },
+      height: {
+      },
+      width: {},
+      lineHeight: {
+      },
+      letterSpacing: ['dark']
+    },
+  },
+  variants: {},
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms")
+  ],
+};
